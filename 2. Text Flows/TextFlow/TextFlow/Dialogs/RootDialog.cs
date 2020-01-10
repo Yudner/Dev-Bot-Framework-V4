ac578f08a3f576487865701a64822bfa69344ce1
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2020-01-09 22:01:32 -0500

    Flujos de texto

---
+++ 2. Text Flows/TextFlow/TextFlow/Dialogs/RootDialog.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextFlow.Dialogs
+{
+    public class RootDialog: ComponentDialog
+    {
+        public RootDialog()
+        {
+            var waterfallStep = new WaterfallStep[]
+            {
+                SetName,
+                SetAge,
+                ShowData
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallStep));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new NumberPrompt<int>(nameof(NumberPrompt<int>), ValidateAge));
+        }
+
+        private async Task<bool> ValidateAge(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(
+              promptContext.Recognized.Succeeded &&
+              promptContext.Recognized.Value > 0 &&
+              promptContext.Recognized.Value < 150
+            );
+        }
+
+        private async Task<DialogTurnResult> SetName(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync("Para iniciar una conversación necesito algunos datos.", cancellationToken: cancellationToken);
+            await Task.Delay(1000);
+            return await stepContext.PromptAsync(
+              nameof(TextPrompt),
+              new PromptOptions { Prompt = MessageFactory.Text("Por favor ingresa tu nombre")},
+              cancellationToken
+            );
+        }
+
+        private async Task<DialogTurnResult> SetAge(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var name = stepContext.Context.Activity.Text;
+            return await stepContext.PromptAsync(
+                nameof(NumberPrompt<int>),
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text($"Bien {name}, ahora necesito tu edad"),
+                    RetryPrompt = MessageFactory.Text($"{name}, Por favor ingresa una edad válida")
+                },
+                cancellationToken
+                );
+        }
+
+        private async Task<DialogTurnResult> ShowData(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync("Genial, gracias por registrar tus datos.", cancellationToken: cancellationToken);
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ 2. Text Flows/TextFlow/TextFlow/TextFlow.cs
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace TextFlow
+{
+    public class TextFlow<T> : ActivityHandler where T: Dialog
+    {
+        protected readonly Dialog _dialog;
+        protected readonly BotState _conversationState;
+        protected readonly ILogger _logger;
+
+        public TextFlow(T dialog, ConversationState conversationState, ILogger<TextFlow<T>> logger)
+        {
+            _dialog = dialog;
+            _conversationState = conversationState;
+            _logger = logger;
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello world!"), cancellationToken);
+                }
+            }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await _dialog.RunAsync(
+              turnContext,
+              _conversationState.CreateProperty<DialogState>(nameof(DialogState)),
+              cancellationToken
+            );
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+            await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+    }
+}
