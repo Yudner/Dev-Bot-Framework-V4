96cba7da28ed48e45f0115a909e8a3698f8bbb6a
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2020-01-10 19:50:47 -0500

    Button Suggestion Action

---
+++ 3. Button Suggestion Action/SuggestAction/SuggestAction/Common/OptionDocumentationDialog.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuggestAction.Common
+{
+    public class OptionDocumentationDialog
+    {
+        public static async Task<DialogTurnResult> ShowOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var options = await stepContext.PromptAsync(
+               nameof(TextPrompt),
+               new PromptOptions
+               {
+                   Prompt = CreateSuggestedActions()
+               },
+               cancellationToken
+            );
+            return options;
+        }
+        private static Activity CreateSuggestedActions()
+        {
+            var reply = MessageFactory.Text("¿De qué plataforma deseas información?");
+
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction(){Title = ".NET", Value = ".NET", Type = ActionTypes.ImBack},
+                    new CardAction(){Title = "JavaScript", Value = "JavaScript", Type = ActionTypes.ImBack},
+                    new CardAction(){Title = "Python", Value = "Python", Type = ActionTypes.ImBack}
+                }
+            };
+            return reply;
+        }
+    }
+}
---
+++ 3. Button Suggestion Action/SuggestAction/SuggestAction/Dialogs/RootDialog.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using SuggestAction.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuggestAction.Dialogs
+{
+    public class RootDialog: ComponentDialog
+    {
+        public RootDialog()
+        {
+            var waterfallStep = new WaterfallStep[]
+            {
+                ShowOptions,
+                ShowDocumentation
+            };
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallStep));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+        }
+
+        private async Task<DialogTurnResult> ShowOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            //MOSTRAR BOTONES
+            return await OptionDocumentationDialog.ShowOptions(stepContext, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ShowDocumentation(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var option = stepContext.Context.Activity.Text;
+            switch (option)
+            {
+                case ".NET":
+                    await stepContext.Context.SendActivityAsync("Documentación .NET: https://docs.microsoft.com/en-us/azure/bot-service/dotnet/bot-builder-dotnet-sdk-quickstart?view=azure-bot-service-4.0", cancellationToken: cancellationToken);
+                    break;
+                case "JavaScript":
+                    await stepContext.Context.SendActivityAsync("Documentación JavaScript: https://docs.microsoft.com/en-us/azure/bot-service/javascript/bot-builder-javascript-quickstart?view=azure-bot-service-4.0", cancellationToken: cancellationToken);
+                    break;
+                case "Python":
+                    await stepContext.Context.SendActivityAsync("Documentación Python: https://docs.microsoft.com/en-us/azure/bot-service/python/bot-builder-python-quickstart?view=azure-bot-service-4.0", cancellationToken: cancellationToken);
+                    break;
+                default:
+                    break;
+            }
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
