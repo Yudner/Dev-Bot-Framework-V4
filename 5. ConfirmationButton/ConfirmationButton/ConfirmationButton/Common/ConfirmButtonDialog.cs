1f054797c5ae8ef9a4024084dfde09ee6bf6d3aa
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2020-01-16 23:58:54 -0500

    Confirmation Button

---
+++ 5. ConfirmationButton/ConfirmationButton/ConfirmationButton/Common/ConfirmButtonDialog.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConfirmationButton.Common
+{
+    public class ConfirmButtonDialog
+    {
+        public static async Task<DialogTurnResult> ShowOption(WaterfallStepContext stepContext, CancellationToken cancellation)
+        {
+            var options = await stepContext.PromptAsync(
+              nameof(ConfirmPrompt),
+              new PromptOptions
+              {
+                  Prompt = MessageFactory.Text("¿Aceptas los términos y condiciones?"),
+                  Style = ListStyle.SuggestedAction
+              }
+            );
+            return options;
+        }
+    }
+}
---
+++ 5. ConfirmationButton/ConfirmationButton/ConfirmationButton/Dialogs/RootDialog.cs
@@ -0,0 +1,45 @@
+﻿using ConfirmationButton.Common;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConfirmationButton.Dialogs
+{
+    public class RootDialog: ComponentDialog
+    {
+        public RootDialog()
+        {
+            var waterfallSteps = new WaterfallStep[]
+            {
+                ShowOptions,
+                ConfirmOptions
+            };
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt), ConfirmValidate));
+        }
+
+        private async Task<DialogTurnResult> ShowOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            //MOSTRAR OPCIONES DE CONFIRMACIÓN
+            return await ConfirmButtonDialog.ShowOption(stepContext, cancellationToken);
+        }
+        private async Task<bool> ConfirmValidate(PromptValidatorContext<bool> promptContext, CancellationToken cancellationToken)
+        {
+            var option = promptContext.Recognized.Value;
+
+            if (option)
+                await promptContext.Context.SendActivityAsync("Tus datos han sido registrados", cancellationToken: cancellationToken);
+            else
+                await promptContext.Context.SendActivityAsync("Está bien, será la próxima", cancellationToken: cancellationToken);
+
+            return true;
+        }
+        private async Task<DialogTurnResult> ConfirmOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
