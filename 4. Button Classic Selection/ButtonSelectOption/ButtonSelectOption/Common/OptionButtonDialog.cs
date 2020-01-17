3080e7cea12c9d9ecd77a8e2e614d6d1e612b9de
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2020-01-16 21:54:28 -0500

    Button Classic Selecction

---
+++ 4. Button Classic Selection/ButtonSelectOption/ButtonSelectOption/Common/OptionButtonDialog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ButtonSelectOption.Common
+{
+    public class OptionButtonDialog
+    {
+        public static async Task<DialogTurnResult> ShowOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var option = await stepContext.PromptAsync(
+              nameof(ChoicePrompt),
+              new PromptOptions
+              {
+                  Prompt = MessageFactory.Text("Selecciona la nube de tu preferencia"),
+                  Choices = ChoiceFactory.ToChoices(new List<string> {"Azure", "AWS", "Google Cloud" }),
+                  Style = ListStyle.HeroCard
+              },
+              cancellationToken
+            );
+            return option;
+        }
+    }
+}
---
+++ 4. Button Classic Selection/ButtonSelectOption/ButtonSelectOption/Dialogs/RootDialog.cs
@@ -0,0 +1,51 @@
+﻿using ButtonSelectOption.Common;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ButtonSelectOption.Dialogs
+{
+    public class RootDialog: ComponentDialog
+    {
+        public RootDialog()
+        {
+            var waterfallSteps = new WaterfallStep[]
+            {
+                ShowOptions,
+                ConfirmOptions
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+        }
+
+        private async Task<DialogTurnResult> ShowOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            //MOSTRAR OPCIONES
+            return await OptionButtonDialog.ShowOptions(stepContext, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ConfirmOptions(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var option = stepContext.Context.Activity.Text;
+            switch (option)
+            {
+                case "Azure":
+                    await stepContext.Context.SendActivityAsync("Seleccionaste Azure", cancellationToken: cancellationToken);
+                    break;
+                case "AWS":
+                    await stepContext.Context.SendActivityAsync("Seleccionaste AWS", cancellationToken: cancellationToken);
+                    break;
+                case "Google Cloud":
+                    await stepContext.Context.SendActivityAsync("Seleccionaste Google Cloud", cancellationToken: cancellationToken);
+                    break;
+                default:
+                    break;
+            }
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
